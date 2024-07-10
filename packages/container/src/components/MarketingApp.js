8fb409b753fcaf6e975974d5b4d0b6c0d84725af
root-ethanhunt <vikash.hunt111@gmail.com>
2024-07-10 14:24:27 +0530

    issue fix 1

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react'
+import React, { useRef, useEffect} from 'react'
 import  MarketingAppcomponents  from 'marketing/MarketingApp';
 import KuchBhi from 'marketing/SomeComp';
 import Landing from 'marketing/Landing';
@@ -10,8 +10,17 @@ console.log(KuchBhi);
  console.log(Landing);
  console.log(MarketingAppcomponents1);
 
+
 function MarketingApp() {
+    const ref = useRef(null);
+
+    // useEffect(()=>{
+    //     MarketingAppcomponents(ref.current);
+    // },[])
   return (
+    // <div ref ={ref}/>
+    // <div>hii</div>
+    //  <MarketingAppcomponents/>
     <MarketingAppcomponents1/>
   )
 }
